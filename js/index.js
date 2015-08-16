3d7d51972eb5f13953e96766ea08c8248c674bf4
Flascher <alexanderflasch@gmail.com>
2015-08-15 22:46:53 -0500

    Polishing more things up. Starting to come together! Also, SVGs, Greensock and Sass are awesome.

--- js/index.js
+++ js/index.js
@@ -26,23 +26,30 @@ $(document).ready(function() {
 		circle.css('height', img.css('height')); //set circle's height to img's height
 		circle.css('width', img.css('width')); //set cricle's width to img's width
 		circle.css('background-color', $('body').css('background-color')); //set circle's color to the body's color
-		circle.css('opacity', '0.6'); //arbitrary value
-		circle.css('border-radius', '100%');
-		circle.css('z-index', '1');
 
 	for(var i = 0; i < 4; i++) {
 		$('.img-container').append(circle.clone().addClass('circle-' + i));
 	}
 
-	var circle0 = $('.circle-0').css('left', '-10px').css('top', '-10px');
-	var circle1 = $('.circle-1').css('left', '-10px').css('top', '10px');
+	var circle0 = $('.circle-0').css('left', '-7.0710678px').css('top', '-7.0710678px');
+	var circle1 = $('.circle-1').css('left', '-7.0710678px').css('top', '7.0710678px');
 	var circle2 = $('.circle-2').css('left', '0px').css('top', '-10px');
 	var circle3 = $('.circle-3').css('left', '-10px').css('top', '0px');
 
 	//ordered by delays. the circle that starts moving first is the first line!
-	TweenMax.from(circle2, 0.5, { top: '10px', ease: Expo.easeInOut, yoyo: true, repeat: -1, delay: 0.125 });
-	TweenMax.from(circle0, 0.5, { left: '10px', top: '10px', ease: Expo.easeInOut, yoyo: true, repeat: -1, delay: 0.25 });
-	TweenMax.from(circle3, 0.5, { left: '10px', ease: Expo.easeInOut, yoyo: true, repeat: -1, delay: 0.375 });
-	TweenMax.from(circle1, 0.5, { left: '10px', top: '-10px', ease: Expo.easeInOut, yoyo: true, repeat: -1, delay: 0.5 });
+	TweenMax.from(circle1, 0.5, { left: '7.0710678px', top: '-7.0710678px', ease: Expo.easeInOut, yoyo: true, repeat: -1, delay: 0.125 });
+	TweenMax.from(circle3, 0.5, { left: '10px', ease: Expo.easeInOut, yoyo: true, repeat: -1, delay: 0.25 });
+	TweenMax.from(circle0, 0.5, { left: '7.0710678px', top: '7.0710678px', ease: Expo.easeInOut, yoyo: true, repeat: -1, delay: 0.375 });
+	TweenMax.from(circle2, 0.5, { top: '10px', ease: Expo.easeInOut, yoyo: true, repeat: -1, delay: 0.5 });
+
+	//arrow animation
+	var arrow = $('.arrow-down');
+
+	var arrowTimeline = new TimelineMax({ delay: 1, repeat: -1, repeatDelay: 2 });
+
+	arrowTimeline.from(arrow, 1, { top: '-10px', opacity: 0.25, ease: Power2.easeOut, yoyo: true, repeat: 5, delay: 1 });
+
+	//animation between header and about me
+
 
 });
